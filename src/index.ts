823d62fac3ad49ee6530a87dc17b9175a0ef43b7
Kerollos Magdy <kerolloz@yahoo.com>
2024-01-18 10:48:23 +0200

    langId to numeric

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ const app = new Elysia()
       body: t.Object({
         name: t.String(),
         sourceCode: t.String(),
-        languageId: t.Number(),
+        languageId: t.Numeric(),
       }),
     }
   )
